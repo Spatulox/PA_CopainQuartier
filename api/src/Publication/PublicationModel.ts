d838b16028f0d0429f0d97c6da592603894329e8
Spatulox <M78stormtrooper@laposte.net>
2025-03-21 15:50:10 +0100

    update Publication models

--- api/src/Publication/PublicationModel.ts
+++ api/src/Publication/PublicationModel.ts
@@ -4,7 +4,7 @@ export interface Publication {
     _id: mongoose.Types.ObjectId,
     name: string,
     created_at: Date,
-    update_at: Date,
+    updated_at: Date,
     author_id: mongoose.Types.ObjectId,
     activity_id?: mongoose.Types.ObjectId,
     troc_id?: mongoose.Types.ObjectId,
