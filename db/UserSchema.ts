024eac414e77b7c60f4b866162d4004dd719c9ce
Spatulox <M78stormtrooper@laposte.net>
2025-03-21 09:40:50 +0100

    add UserSchema

---
+++ db/UserSchema.ts
@@ -0,0 +1,16 @@
+import { Schema, model } from 'mongoose';
+
+export const UserSchema = new Schema({
+    name: String,
+    lastname: String,
+    email: String,
+    address: String,
+    verified: Boolean,
+    role: String,
+    group_chat_list_id: Array<Number>,
+    troc_score: Number,
+    phone: String,
+});
+
+
+const User = model('User', UserSchema);
