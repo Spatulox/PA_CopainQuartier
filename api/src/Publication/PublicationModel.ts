42963427360d01d0b4d677772ec50464e296c0dd
Spatulox <M78stormtrooper@laposte.net>
2025-03-21 16:29:15 +0100

    update Publication, remove the troc_id link

--- api/src/Publication/PublicationModel.ts
+++ api/src/Publication/PublicationModel.ts
@@ -7,6 +7,5 @@ export interface Publication {
     updated_at: Date,
     author_id: mongoose.Types.ObjectId,
     activity_id?: mongoose.Types.ObjectId,
-    troc_id?: mongoose.Types.ObjectId,
     body: string
 }
--- api/src/db/PublicationSchema.ts
+++ api/src/db/PublicationSchema.ts
@@ -7,18 +7,9 @@ const PublicationSchema = new Schema<Publication>({
     updated_at: { type: Date, default: null },
     author_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
     activity_id : { type: Schema.Types.ObjectId, ref: "Activity", required: false },
-    troc_id: { type: Schema.Types.ObjectId, ref: "Troc", required: false },
     body: {type: String, required: true}
 }, {
     timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
-});
-
-PublicationSchema.pre('validate', function(next) {
-    if (this.activity_id && this.troc_id) {
-        next(new Error('Une publication ne peut pas être liée à la fois à une activité et à un troc.'));
-    } else {
-        next();
-    }
 });
 
 export const PublicationRepository = model('Publication', PublicationSchema);
