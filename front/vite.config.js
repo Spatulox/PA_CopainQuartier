f103223fe6abcf4654ae250787bd52c51be338e0
Spatulox <M78stormtrooper@laposte.net>
2025-04-12 16:17:07 +0200

    add basic conf for ts and react

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import config from './src/config.json'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': config.baseUrl
+    }
+  }
 })
