c4452c33158f4a670451dee2f33dd1344981f574
Spatulox <M78stormtrooper@laposte.net>
2025-04-12 17:06:54 +0200

    fix des trucs

--- front/src/app/App.tsx
+++ front/src/app/App.tsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import '../css/App.css'
+// App.tsx
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './shared/header';
+import Footer from './shared/footer';
+import AuthForm from './pages/auth_register';
+import HomePage from './pages/home_page';
+
+import './css/App.css'
+import './css/index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src="" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src="" className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthForm />} />
+        {/* ... autres routes */}
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App
