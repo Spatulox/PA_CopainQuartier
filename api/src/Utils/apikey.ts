c4592c62d91f36b275d75a276c5b00dc094d366e
Spatulox <M78stormtrooper@laposte.net>
2025-04-30 14:13:10 +0200

    remove ApiKey() decorator

--- api/src/Utils/apikey.ts
+++
@@ -1,8 +0,0 @@
-import { createParamDecorator } from 'routing-controllers';
-
-export function ApiKey() {
-    return createParamDecorator({
-      required: true,
-      value: action => action.request.headers['apikey']
-    });
-  }
