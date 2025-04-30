83766aebf793379a2ad2a222a729d3ef5113d2ff
Spatulox <M78stormtrooper@laposte.net>
2025-04-30 14:14:59 +0200

    Implements account retrieval and deletion
    Adds endpoints for users to retrieve their own account
    information and to delete their account.

--- api/src/Controllers/AccountController.ts
+++ api/src/Controllers/AccountController.ts
@@ -1,12 +1,19 @@
 import { JsonController, Param, Body, Get, Post, Put, Delete, HeaderParam, CurrentUser, Authorized } from 'routing-controllers';
 import { User } from "../Models/UserModel"
+import { deleteMyAccount, getUserById } from '../Services/users/usersPublic';
 
 @JsonController()
 export class UserController {
 
   @Get('/account')
   @Authorized()
-  getAll(@CurrentUser() user: User) {
-    return true;
+  async getMyAccount(@CurrentUser() user: User): Promise<User | null> {
+    return await getUserById(user._id);
+  }
+
+  @Delete('/account')
+  @Authorized()
+  async deleteMyAccount(@CurrentUser() user: User): Promise<boolean>{
+    return await deleteMyAccount(user);
   }
 }
