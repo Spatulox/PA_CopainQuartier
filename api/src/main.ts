c8def1e4c2c8605ca08b35199e88a923e4dc6983
Spatulox <M78stormtrooper@laposte.net>
2025-05-02 13:37:23 +0200

    Update api/src/main.ts
    Registers controllers and configures WebSocket server
    to handle real-time channel communication.

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,18 @@
+import 'reflect-metadata';
 import { createExpressServer, RoutingControllersOptions } from 'routing-controllers';
 import { UserController } from './Controllers/UserController';
 import { connectDB } from './DB_Schema/connexion';
 import { ErrorHandler } from './Middleware/error-handling';
 import { authMiddleware, getCurrentUser } from './Middleware/auth';
+import { ActivityController } from './Controllers/ActivitiesController';
+import { AccountController } from './Controllers/AccountController';
+import { ChannelsController } from './Controllers/ChannelsControllers';
+import { PublicationsController } from './Controllers/PublicationsController';
+import { TrocController } from './Controllers/TrocsController';
+import { WebSocketServer } from 'ws';
+import http from 'http';
+import { handleMessage } from './Controllers/ChannelsWebsoketController';
+import { parse } from 'url';
 
 
 async function main(){
@@ -15,12 +25,45 @@ async function main(){
     authorizationChecker: authMiddleware,
     currentUserChecker: getCurrentUser,
     middlewares: [ErrorHandler],
-    controllers: [UserController],
+    controllers: [AccountController,
+      ActivityController,
+      ChannelsController,
+      PublicationsController,
+      TrocController,
+      UserController],
     defaultErrorHandler: false
   }
 
   const app = createExpressServer(routingControllerOptions);
-  app.listen(port);
+  const server = http.createServer(app)
+  const wss = new WebSocketServer({ server });
 
+  server.listen(port);
+  wss.on('connection', (ws, req) => {
+    console.log('Client connecté');
+  
+    const pathname = parse(req.url || '').pathname;
+    if(!pathname){
+      console.log("wtf")
+      return
+    }
+    const parts = pathname.split('/');
+    const channelId = parts[parts.length - 1];
+
+    // Check inside db if channel_id exist
+    // CheckChannelExist(channelId)
+
+    // Check si l'utilisateur à le droit d'accéder au channel
+    // Récupère les messages du channel
+    // Envoie les messages au client
+  
+    ws.on('message', (data) => {
+      handleMessage(wss, ws, data, channelId);
+    });
+  
+    ws.on('close', () => {
+      console.log('Client déconnecté');
+    });
+  });
 }
 main()
