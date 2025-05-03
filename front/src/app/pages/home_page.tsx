f63fae86043cd30f56d5bd0a933fea240623652e
Spatulox <M78stormtrooper@laposte.net>
2025-05-03 18:00:49 +0200

    Updates home_page.tsx layout
    Adds initial layout and placeholders for summaries on the home page.

--- front/src/app/pages/home_page.tsx
+++ front/src/app/pages/home_page.tsx
@@ -1,7 +1,15 @@
 // app/pages/home_page.tsx
 function HomePage() {
     return (
-      <div>Home Page</div>
+      <>
+        <h1>Home Page</h1>
+        <div>
+          <h2>Récapitulatifs</h2>
+          <h3>Publications</h3>
+          <h3>Actualités</h3>
+          <h3>Trocs</h3>
+        </div>
+      </>
     );
 }
 
