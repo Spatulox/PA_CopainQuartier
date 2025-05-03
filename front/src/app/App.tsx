0beb197ce6be4f5784c34aab001c61b9b8b4dc9e
Spatulox <M78stormtrooper@laposte.net>
2025-05-03 16:45:14 +0200

    Updates App.tsx: Adds auth and chat routes
    Adds AuthProvider for user authentication.
    Integrates chat route with and without ID parameters.

--- front/src/app/App.tsx
+++ front/src/app/App.tsx
@@ -1,26 +1,33 @@
 // App.tsx
+import { AuthProvider } from "./shared/auth-context";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './shared/header';
 import Footer from './shared/footer';
 import AuthForm from './pages/auth_register';
 import Account from './pages/account';
 import HomePage from './pages/home_page';
+import Chat from './pages/chat';
+import { Route as CRoute } from './constantes';
 
 import './css/App.css'
 import './css/index.css'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/auth" element={<AuthForm />} />
-        <Route path="/account" element={<Account />} />
-        {/* ... autres routes */}
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path={CRoute.base} element={<HomePage />} />
+          <Route path={CRoute.auth} element={<AuthForm />} />
+          <Route path={CRoute.account} element={<Account />} />
+          <Route path={`${CRoute.chat}`} element={<Chat />} />
+          <Route path={`${CRoute.chat}/:id`} element={<Chat />} />
+          {/* ... autres routes */}
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
