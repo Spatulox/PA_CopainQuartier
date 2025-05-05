24c75f711c019c8cbdc0fd08cf2472760093eda7
Spatulox <M78stormtrooper@laposte.net>
2025-05-05 10:27:28 +0200

    Update main.ts: Registers admin controllers
    Registers the admin controllers for trocs and users,
    making them available in the application.

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import { createExpressServer, RoutingControllersOptions } from 'routing-controllers';
-import { UserController } from './Controllers/UserController';
+import { AdminUserController, UserController } from './Controllers/UserController';
 import { connectDB } from './DB_Schema/connexion';
 import { ErrorHandler } from './Middleware/error-handling';
 import { authMiddleware, getCurrentUser } from './Middleware/auth';
@@ -8,7 +8,7 @@ import { ActivityController } from './Controllers/ActivitiesController';
 import { AccountController } from './Controllers/AccountController';
 import { ChannelsController } from './Controllers/ChannelsControllers';
 import { PublicationsController } from './Controllers/PublicationsController';
-import { TrocController } from './Controllers/TrocsController';
+import { AdminTrocController, TrocController } from './Controllers/TrocsController';
 import { WebSocketServer } from 'ws';
 import http from 'http';
 import { channelSubscriptions, handleMessage, initAccessMap } from './Controllers/ChannelsWebsoketController';
@@ -26,7 +26,11 @@ async function main(){
     authorizationChecker: authMiddleware,
     currentUserChecker: getCurrentUser,
     middlewares: [ErrorHandler],
-    controllers: [AccountController,
+    controllers: [
+      AdminTrocController,
+      AdminUserController,
+
+      AccountController,
       ActivityController,
       AuthController,
       ChannelsController,
