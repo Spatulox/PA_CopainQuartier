cbe1d8fca04f98c75669006c3e3254dfd188d276
Spatulox <M78stormtrooper@laposte.net>
2025-05-05 20:01:25 +0200

    Updates api/chat.ts
    Adds methods to delete, leave, and join channels.
    
    Corrects typo in Channel type's description field.

--- front/src/api/chat.ts
+++ front/src/api/chat.ts
@@ -7,22 +7,38 @@ export type Message = {
 
 export type Channel = {
     _id: string;
+    activity_id: string
+    admin_id: string
     name: string;
     pulication_id: string | null,
     type: string,
-    decription: string,
+    description: string,
     created_at: string
+    member_auth: string,
+    member: string[]
+    messages: string[]
   };
-  
 
 export class Chat extends ApiClient {
     async getChannelById(id: string): Promise<Channel> {
-        const response = await this.client.get(`/channels/${id}`);
+        const response = await this.Get(`/channels/${id}`);
         return response.data;
     }
 
     async getChannel(): Promise<Channel[]> {
-        const response = await this.client.get(`/channels/@me`);
+        const response = await this.Get(`/channels/@me`);
         return response.data;
+    }
+
+    async deleteChat(channel_id: string): Promise<boolean>{
+      return await this.Delete(`/channels/${channel_id}`)
+    }
+
+    async leaveChat(id: string): Promise<boolean>{
+      return await this.Patch(`/channels/${id}/removeuser/${this.user?._id}`, {})
+    }
+
+    async joinChat(channel_id: string, user_id_to_add: string): Promise<boolean>{
+      return await this.Patch(`/channels/${channel_id}/adduser/${user_id_to_add}`, {})
     }
 }
