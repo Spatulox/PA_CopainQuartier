c170d1451d27df146587d100bf33ba96b2bce072
Spatulox <M78stormtrooper@laposte.net>
2025-05-06 14:54:13 +0200

    Implements Activity list and creation
    Adds components for displaying and creating activities.
    Fetches activities from the API and displays them in a list.
    Includes a form for creating new activities.

--- front/src/app/components/Activity/ActivityList.tsx
+++ front/src/app/components/Activity/ActivityList.tsx
@@ -1,5 +1,46 @@
-function ActivityList(){
-    return <h2>ActivityList</h2>
+import { act, useEffect, useState } from "react";
+import { Activity, ActivityClass } from "../../../api/activity";
+import CreateActivity from "./CreateActivity";
+import { UserClass } from "../../../api/user";
+
+type ActivityListMessage = {
+    message: string
+  }
+
+function ActivityList({message}: ActivityListMessage){
+
+    const [activity, setActivity] = useState<Activity[]>([])
+
+
+    useEffect(() => {
+        (async () => {
+            const client = new ActivityClass()
+            const activities = await client.getActivities()
+            console.log(activities)
+            setActivity(activities)
+        })()
+    }, [message])
+
+    return <>
+        <h2>Activités</h2>
+        <section>
+        { activity?.length == 0 ? (<p>Aucune Activités trouvée</p>) : (
+            activity?.map((acti) => (
+                <div key={acti._id}>
+                    <h3>{acti.title}</h3>
+                    <span>
+                        Auteur : {acti.author_id?.email ? acti.author_id.email : "Unknow" } - Date de création : {new Date(acti.created_at).toLocaleDateString()}
+                    </span>
+                    <p>{acti.description}</p>
+                    <span>
+                        Date de participation : {new Date(acti.date_reservation).toLocaleDateString()}
+                    </span><br></br>
+                    <button id={acti.publication._id}>Voir la publication : {acti.publication.name}</button>
+                </div>
+            )))
+        }
+        </section>
+    </>
 }
 
 export default ActivityList;
--- front/src/app/components/Activity/ActivityPage.tsx
+++ front/src/app/components/Activity/ActivityPage.tsx
@@ -1,7 +1,26 @@
 // app/pages/activity.tsx
 
+import { useState } from "react";
+import ActivityList from "./ActivityList";
+import CreateActivity from "./CreateActivity";
+import { useNavigate } from "react-router-dom";
+import { Route } from "../../constantes";
+
 function Activity(){
-    return <h2>Activity</h2>
+
+    const [message, setMessage] = useState("");
+    const handleUpdate = (newMsg:string) => {
+        setMessage(newMsg);
+    };
+    const navigate = useNavigate()
+
+    return <>
+    <ActivityList message={message} />
+    <div>
+        <CreateActivity onUpdate={handleUpdate} />
+        <button onClick={() => navigate(Route.manageActivity)}>Gérer mes Activités</button>
+    </div>
+    </>
 }
 
 export default Activity;
