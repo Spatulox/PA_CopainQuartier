6937c9e00ae32330331d53d787d85bed814fa4fb
Spatulox <M78stormtrooper@laposte.net>
2025-05-12 14:42:15 +0200

    Update PublicationManage.tsx
    Implements the display of user publications with management options.
    
    Fetches and displays a list of publications associated with the user.
    Provides navigation buttons for managing individual publications and related activities.
    Adds loading and empty state handling for better user experience.

--- front/src/app/components/Publications/PublicationManage.tsx
+++ front/src/app/components/Publications/PublicationManage.tsx
@@ -4,7 +4,42 @@ import { Publication, PublicationClass } from "../../../api/publications";
 import { Route } from "../../constantes";
 
 export function ManageMyPublications(){
-    return <><h1>My Publi</h1></>
+    const [publications, setPublications] = useState<Publication[] | null>(null);
+    const navigate = useNavigate()
+    
+    useEffect(() => {
+        (async () => {
+        const client = new PublicationClass();
+        const pub = await client.getMyPublications();
+        console.log(pub)
+        setPublications(pub);
+        })();
+    }, []);
+    
+    if (publications === null) {
+        return <div>Chargement des publications...</div>;
+    }
+    
+    if (publications.length === 0) {
+        return <div>Aucune publications trouvée.</div>;
+    }
+    
+    return <>    
+        <h2>Mes Publications</h2>
+        <section>{publications.map((pub) => (
+            <div key={pub._id}>
+                <h3>{pub.name}</h3>
+                <span>{new Date(pub.created_at).toLocaleDateString()}</span>
+                <p>{pub.body}</p>
+                <div>
+                    <button onClick={() => navigate(`${Route.managePublications}/${pub._id}`)}>Gérer la publications</button>
+                    {pub.activity_id ?
+                        <button onClick={() => navigate(`${Route.manageActivity}/${pub.activity_id}`)}>Gérer l'activité lié</button>: ""
+                    }
+                </div>
+            </div>
+        ))}</section>
+    </>
 }
 
 function ManagePublicationAdmin(){
@@ -16,7 +51,6 @@ function ManageOnePublication(){
 }
 
 function ManagePublication(){
-    const [publications, setPublications] = useState<Publication[] | null>(null);
     const { id } = useParams<{ id: string }>();
     const [userIsAdmin, setUserAdmin] = useState(false)
     const navigate = useNavigate()
@@ -34,9 +68,6 @@ function ManagePublication(){
         }
     }, [userIsAdmin, navigate]);
 
-    if(!publications){
-        return <p>Chargement des publications</p>
-    }
 
     if(id){
         return <><ManageOnePublication /></>
