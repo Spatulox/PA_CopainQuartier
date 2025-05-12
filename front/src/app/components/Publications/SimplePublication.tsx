027bdcdd1321fce19af9d38c94344166956ef02a
Spatulox <M78stormtrooper@laposte.net>
2025-05-12 16:17:42 +0200

    Refactors SimplePublication component
    Updates the SimplePublication component to allow for more granular control over button visibility.
    
    Introduces a `ShowPublicationButton` enum to define which buttons (View Activity, View Publication, Manage) should be displayed.
    
    This allows for more flexible use of the component in different contexts.

--- front/src/app/components/Publications/SimplePublication.tsx
+++ front/src/app/components/Publications/SimplePublication.tsx
@@ -1,18 +1,33 @@
 import Publications from "./PublicationsPage";
 import { Publication } from "../../../api/publications";
 import { User } from "../../../api/user";
+import { Activity } from "../../../api/activity";
+
+export enum ShowPublicationButton {
+    Publication = 1 << 0,        // 1 (0b001)
+    ViewActivity = 1 << 1, // 2 (0b010)
+    Manage = 1 << 2,          // 4 (0b100)
+    All = ViewActivity | Publication | Manage, // 7
+    None = 0
+}
+
+type ShowPublicationProps = {
+    pub: Publication,
+    user: User | null,
+    onViewPublication?: (id: string) => void,
+    onViewActivity?: (id: string) => void,
+    onManage?: (id: string) => void,
+    buttonShow: ShowPublicationButton
+}
 
 export function ShowPublication({
     pub,
     user,
-    onView,
-    onManage
-}: {
-    pub: Publication,
-    user: User | null,
-    onView?: (id: string) => void,
-    onManage?: (id: string) => void
-}) {
+    onViewPublication,
+    onViewActivity,
+    onManage,
+    buttonShow
+}: ShowPublicationProps) {
     return (
         <div key={pub._id}>
             <div>
@@ -25,10 +40,37 @@ export function ShowPublication({
                     ? pub.author_id.name
                     : pub.author_id?.toString()}
                 <div>
-                    {onView != null ? <button onClick={() => onView(pub._id)}>Voir la Publication</button> : "" }
-                    {onManage != null && typeof pub.author_id === "object" && pub.author_id !== null && user?._id === pub.author_id?._id &&
-                        <button onClick={() => onManage(pub._id)}>Gérer la publication</button>
-                    }
+                    {/* Bouton Voir l'activité associée */}
+                    {(buttonShow & ShowPublicationButton.ViewActivity) !== 0 &&
+                        onViewActivity &&
+                        typeof pub.activity_id === "object" &&
+                        pub.activity_id !== null &&
+                        "_id" in pub.activity_id && (
+                            <button
+                                id={(pub.activity_id as Activity)._id}
+                                onClick={() => onViewActivity((pub.activity_id as Activity)._id)}
+                            >
+                                Voir l'activité associée
+                            </button>
+                    )}
+
+                    {/* Bouton Voir la publication */}
+                    {(buttonShow & ShowPublicationButton.Publication) !== 0 && onViewPublication && (
+                        <button id={pub._id} onClick={() => onViewPublication(pub._id)}>
+                            Voir la Publication
+                        </button>
+                    )}
+
+                    {/* Bouton Gérer */}
+                    {(buttonShow & ShowPublicationButton.Manage) !== 0 &&
+                        onManage &&
+                        typeof pub.author_id === "object" &&
+                        pub.author_id !== null &&
+                        user?._id === pub.author_id?._id && (
+                            <button onClick={() => onManage(pub._id)}>
+                                Gérer la publication
+                            </button>
+                    )}
                 </div>
             </div>
         </div>
