ab544aa3d214f3d024918c1597815a9da3ab067a
Spatulox <M78stormtrooper@laposte.net>
2025-05-12 12:17:26 +0200

    Updates PublicationCreate: Updates form fields
    Updates the publication creation form to align with backend changes.
    Specifically, renames "title" to "name" and "contenu" to "body" for consistency.

--- front/src/app/components/Publications/PublicationCreate.tsx
+++ front/src/app/components/Publications/PublicationCreate.tsx
@@ -8,29 +8,31 @@ type CreatePublicationType = {
 function CreatePublication({onUpdate}: CreatePublicationType){
 
     const fields: FieldForm[] = [
-        { name: "title", label: "Nom de la publication", type: "text", required: true },
+        { name: "name", label: "Nom de la publication", type: "text", required: true },
         { name: "description", label: "Description", type: "text", required: true },
-        { name: "contenu", label: "Contenu", type: "textarea", required: true },
+        { name: "body", label: "Contenu", type: "textarea", required: true },
     ];
     
-    type ActivityForm = {
-        title: string,
+    type PublicationForm = {
+        name: string,
         description: string,
+        body: string,
     };
 
-    async function handleCreateChannel(formData: ActivityForm): Promise<void> {
+    async function handleCreateChannel(formData: PublicationForm): Promise<void> {
         const client = new PublicationClass()
-        if(true){
-            alert("Publication créé !\nNom: " + formData.title + "\nDescription: " + formData.description);
+        if(client){
+            await client.createPublication(formData)
+            alert("Publication créé !\nNom: " + formData.name + "\nDescription: " + formData.description);
             onUpdate("update")
         }
     }
     
     return (
-    <PopupForm<ActivityForm>
+    <PopupForm<PublicationForm>
         title="Créer une Publication"
         fields={fields}
-        initialFormData={{ title: "", description: ""}}
+        initialFormData={{ name: "", description: "", body: ""}}
         onSubmit={handleCreateChannel}
         submitLabel="Créer"
         buttonLabel="Créer une Publication"
