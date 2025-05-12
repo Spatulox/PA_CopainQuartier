2c60da8e378c5337045fe31f90fbbad499573724
Spatulox <M78stormtrooper@laposte.net>
2025-05-12 14:42:26 +0200

    Update PublicationsList component
    Updates the publication list to fetch user data and display a "Manage Publication" button for the author of each publication.
    
    Improves the loading state display and handles cases where no publications are found.

--- front/src/app/components/Publications/PublicationsList.tsx
+++ front/src/app/components/Publications/PublicationsList.tsx
@@ -1,41 +1,58 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Publication, PublicationClass } from "../../../api/publications";
+import { User } from "../../../api/user";
+import { useNavigate } from "react-router-dom";
+import { Route } from "../../constantes";
 
 type PublicationListMessage = {
     message: string
   }
 
 function PublicationList({message}: PublicationListMessage){
-    const [publications, setPublications] = useState<Publication[]>([])
+    const [publications, setPublications] = useState<Publication[] | null>(null)
+    const [user, setUser] = useState<User>(null)
+    const navigate = useNavigate()
 
 
     useEffect(() => {
         (async () => {
             console.log('useEffect')
             const client = new PublicationClass()
+            const use = await client.getMe()
             const pub = await client.getAllPublications()
-            console.log(pub)
             if(pub){
                 setPublications(pub)
+            }
+            if(use){
+                setUser(use)
             }
         })()
     }, [message])
 
+    if (publications === null) {
+        return <div>Chargement des publications...</div>;
+    }
+
     if(publications.length == 0){
-        return <p>Chargement des publications ou pas de publications...</p>
+        return <p>Pas de publications trouvée...</p>
     }
 
     return <>    
         <h2>Publications</h2>
         <section>{publications.map((pub) => (
             <div key={pub._id}>
-                <h3>{pub.name}</h3>
-                <span>{new Date(pub.created_at).toLocaleDateString()}</span>
+                <div>
+                    <h3>{pub.name}</h3>
+                    <span>{new Date(pub.created_at).toLocaleDateString()}</span>
+                </div>
                 <p>{pub.body}</p>
                 <div>
                     {typeof pub.author_id === "object" && pub.author_id !== null
                         ? pub.author_id.name
                         : pub.author_id?.toString()}
+                    {typeof pub.author_id === "object" && pub.author_id !== null && user?._id == pub.author_id?._id ?
+                    <button onClick={() => navigate(`${Route.managePublications}/${pub._id}`)}>Gérer la publication</button>:""
+                    }
                 </div>
             </div>
         ))}</section>
