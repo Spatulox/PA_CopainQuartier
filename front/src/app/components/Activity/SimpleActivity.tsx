c94639efe66eafcbd27778750e121014404f7689
Spatulox <M78stormtrooper@laposte.net>
2025-05-12 15:33:21 +0200

    Updates Activity component
    Displays activity participants.
    Allows admins to manage activities.

--- front/src/app/components/Activity/SimpleActivity.tsx
+++ front/src/app/components/Activity/SimpleActivity.tsx
@@ -2,7 +2,9 @@
 
 import React from "react";
 import { Activity } from "../../../api/activity";
-import { User } from "../../../api/user";
+import { User, UserRole } from "../../../api/user";
+import { Route } from "../../constantes";
+import { useNavigate } from "react-router-dom";
 
 type ShowActivityProps = {
     activity: Activity;
@@ -11,40 +13,49 @@ type ShowActivityProps = {
     onManage?: (id: string) => void;
 };
 
-export function ShowActivity({
-    activity,
-    user,
-    onViewPublication,
-    onManage
-}: ShowActivityProps) {
-    if (!activity) return <>Loading</>;
+export function ShowActivity({ activity, user, onViewPublication, onManage }: ShowActivityProps) {
+    const navigate = useNavigate();
 
     return (
-        <div key={activity._id}>
-            <h2>{activity.title}</h2>
-            <div>
-                Créée le {new Date(activity.created_at).toLocaleDateString()}<br />
-                Réservation : {new Date(activity.date_reservation).toLocaleString()}
-            </div>
-            <div>
-                <strong>Description :</strong>
-                <div>{activity.description}</div>
-            </div>
-            <div>
-                <strong>Auteur :</strong> {activity.author_id?.name}
-            </div>
-            <div>
-                <strong>Publication :</strong> {activity.publication?.name}
-                {onViewPublication && activity.publication?._id && (
-                    <button onClick={() => onViewPublication(activity.publication._id)}>
-                        Voir la Publication associée
-                    </button>
-                )}
-                {onManage && activity.author_id?._id === user?._id && (
-                    <button onClick={() => onManage(activity._id)}>
-                        Gérer l'activité
-                    </button>
-                )}
+        <div>
+            <div key={activity._id}>
+                <h2>{activity.title}</h2>
+                <div>
+                    Créée le {new Date(activity.created_at).toLocaleDateString()}<br />
+                    Réservation : {new Date(activity.date_reservation).toLocaleString()}
+                </div>
+                <div>
+                    <strong>Description :</strong>
+                    <div>{activity.description}</div>
+                </div>
+                <div>
+                    <strong>Auteur :</strong> {activity.author_id?.name}
+                </div>
+                <div>
+                    <strong>Participants ({activity.participants?.length || 0}) :</strong>
+                    <ul>
+                        {activity.participants && activity.participants.length > 0 ? (
+                            activity.participants.map((user) => (
+                                <li key={user?._id}>{user?.name}</li>
+                            ))
+                        ) : (
+                            <li>Rien</li>
+                        )}
+                    </ul>
+                </div>
+                <div>
+                    <strong>Publication :</strong> {activity.publication?.name}
+                    {onViewPublication && activity.publication?._id && (
+                        <button onClick={() => onViewPublication(activity.publication._id)}>
+                            Voir la Publication associée
+                        </button>
+                    )}
+                    {onManage && user && (activity.author_id?._id === user._id || user.role == UserRole.admin) && (
+                        <button onClick={() => onManage(activity._id)}>
+                            Gérer l'activité
+                        </button>
+                    )}
+                </div>
             </div>
         </div>
     );
