9e78cade68bfd7016fc234fc4f4b6466623ab86a
Spatulox <M78stormtrooper@laposte.net>
2025-05-12 18:36:36 +0200

    Creates Trocs page
    Adds the Trocs page with components for listing and creating Trocs.
    The page includes navigation to manage user's Trocs.

--- front/src/app/components/Trocs/TrocsPage.tsx
+++ front/src/app/components/Trocs/TrocsPage.tsx
@@ -1,7 +1,28 @@
 // app/pages/trocs.tsx
 
+import { useState } from "react";
+import CreateTroc from "./TrocCreate";
+import TrocList from "./TrocsList";
+import { useNavigate } from "react-router-dom";
+import { Route } from "../../constantes";
+
 function Trocs(){
-    return <h2>Trocs</h2>
+    const [message, setMessage] = useState<string>("")
+    const navigate = useNavigate()
+    
+    const handleUpdate = (newMsg: string) => {
+        setMessage(newMsg);
+    };
+    
+    return <>
+        <TrocList message={message}/>
+        <div>
+            <CreateTroc onUpdate={handleUpdate} />
+            <button onClick={() => navigate(Route.manageMyTrocs)}>
+                Gérer mes Trocs
+            </button>
+        </div>
+    </>
 }
 
 export default Trocs;
