1b0de7c7dea466884d9274f2bad30157b8159d51
Spatulox <M78stormtrooper@laposte.net>
2025-05-14 21:16:35 +0200

    Refactors trocs validation schema
    Moves the approval schema to the utils file to increase the reusability of the function.
    Removes unused type.

--- api/src/Validators/utils.ts
+++ api/src/Validators/utils.ts
@@ -2,3 +2,10 @@ import {z} from "zod";
 
 export const zId = z.coerce.number().int().positive();
 export const zObjectId = z.string().regex(/^[0-9a-fA-F]{24}$/, "Invalid ObjectId");
+
+
+
+export const zApprove = z.object({
+  approve: z.boolean()
+});
+export type ApproveBody = z.infer<typeof zApprove>
