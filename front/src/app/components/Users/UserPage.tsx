fe054239639025d860d0b12364e8b36929938899
Spatulox <M78stormtrooper@laposte.net>
2025-05-14 21:50:35 +0200

    Creates UserPage component
    Adds a new component to display user information based on ID,
    handles admin/user roles for data fetching,
    and includes loading/error states.

---
+++ front/src/app/components/Users/UserPage.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react"
+import { AdminUserClass, User, UserClass } from "../../../api/user"
+import { useNavigate, useParams } from "react-router-dom";
+import { ShowUser, ShowUserButton } from "./SimpleUser";
+import { Route } from "../../constantes";
+import Loading from "../shared/loading";
+
+function Users(){
+    const { id } = useParams<{ id: string }>();
+    const [user, setUser] = useState<User | null>(null)
+    const [me, setMe] = useState<User | null>(null)
+    const [isAdmin, setAdmin] = useState<boolean>(false)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        (async () => {
+            if(id){
+                if(!isAdmin){
+                    const client = new UserClass()
+                    await client.refreshUser()
+                    const use = await client.getUserByID(id)
+                    if(isAdmin != client.isAdmin()){
+                        setAdmin(client.isAdmin())
+                        return
+                    }
+                    setUser(use)
+                    const me = await client.getMe()
+                    setMe(me)
+                } else if(isAdmin){
+                    const client = new AdminUserClass()
+                    const use = await client.getUserByID(id)
+                    setUser(use)
+                    const me = await client.getMe()
+                    setMe(me)
+                }
+            }
+        })()
+    }, [id, isAdmin])
+
+    if(!user && !me){
+        return <Loading title="Chargement de l'utilisateur" />
+    }
+
+    if(!user && me){
+        return <p>Cet utilisateur n'existe pas</p>
+    }
+
+    const handleDelete = (id: string) => {
+        console.log(id)
+    }
+
+    const handleApprove = (id: string, bool: boolean) => {
+        console.log(id)
+    }
+
+    return <>
+        <ShowUser
+        theuser={user}
+        user={me}
+        onDelete={handleDelete}
+        onApprove={handleApprove}
+        onManage={(id) => navigate(`${Route.manageUser}/${id}`)}
+        buttonShow={ShowUserButton.Delete | ShowUserButton.Approve | ShowUserButton.Manage}
+        />
+    </>
+}
+
+export default Users
