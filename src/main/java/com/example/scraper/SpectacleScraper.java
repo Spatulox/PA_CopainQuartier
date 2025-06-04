f2e2fe2eb2d6743c90399184edbf96b6186f26d8
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2025-06-04 12:41:03 +0200

    add spectacle

---
+++ src/main/java/com/example/scraper/SpectacleScraper.java
@@ -0,0 +1,7 @@
+package com.example.scraper;
+
+public class SpectacleScraper extends CategoryScraper {
+    public SpectacleScraper() {
+        super("https://www.evous.fr/paris/Spectacles/", "spectacle");
+    }
+}
