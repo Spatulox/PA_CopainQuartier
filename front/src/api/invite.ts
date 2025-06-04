54aac76cbefe1c1ecc7a5b488c271b444bda4105
Spatulox <M78stormtrooper@laposte.net>
2025-06-04 19:15:31 +0200

    Updates invite API
    Renames parameters for clarity and consistency.
    Returns the invite ID in generateInvite.

--- front/src/api/invite.ts
+++ front/src/api/invite.ts
@@ -15,20 +15,20 @@ export type PublicInvite = {
 export class InviteClass extends ApiClient {
     protected url ="/invites"
 
-    async getInvite(id: string): Promise<Invite>{
-        return await this.Get(`${this.url}/${id}`)
+    async getInvite(invite_id: string): Promise<Invite>{
+        return await this.Get(`${this.url}/${invite_id}`)
     }
 
-    async generateInvite(id: string): Promise<string>{
-        let res = await this.Post(`${this.url}`, {channel_id: id})
+    async generateInvite(id_to_link_to_an_invite: string): Promise<string>{
+        let res = await this.Post(`${this.url}`, {channel_id: id_to_link_to_an_invite})
         if("channel_id" in res){
             res = res as PublicInvite
-            return `${this.baseURL}${this.url}/${res.channel_id}`
+            return `${this.baseURL}${this.url}/${res._id}`
         }
         return res
     }
 
-    async joinByInvite(id: string): Promise<void>{
-        return await this.Post(`${this.url}/${id}`, {})
+    async joinByInvite(invite_id: string): Promise<void>{
+        return await this.Post(`${this.url}/${invite_id}`, {})
     }
 }
