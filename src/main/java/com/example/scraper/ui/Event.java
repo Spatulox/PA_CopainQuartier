8a05be5e3f6f884204d0fa2853aeb4967cb2e582
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2025-06-04 12:42:45 +0200

    chang class event

--- src/main/java/com/example/scraper/ui/Event.java
+++ src/main/java/com/example/scraper/ui/Event.java
@@ -6,10 +6,12 @@ import javafx.beans.property.StringProperty;
 public class Event {
     private final StringProperty title;
     private final StringProperty date;
+    private final StringProperty url;
 
-    public Event(String title, String date) {
+    public Event(String title, String date, String url) {
         this.title = new SimpleStringProperty(title);
         this.date = new SimpleStringProperty(date);
+        this.url = new SimpleStringProperty(url);
     }
 
     public StringProperty titleProperty() {
@@ -18,5 +20,13 @@ public class Event {
 
     public StringProperty dateProperty() {
         return date;
+    }
+
+    public StringProperty urlProperty() {
+        return url;
+    }
+
+    public String getUrl() {
+        return url.get();
     }
 }
