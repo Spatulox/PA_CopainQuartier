554c7c28b13d4f0f56379c233ca71789ef8a85fb
Spatulox <M78stormtrooper@laposte.net>
2025-06-17 13:53:42 +0200

    Updates UserSchema and UserModel
    Updates the `friends_id` field in the UserSchema to use a Map instead of an array for improved efficiency.
    Reflects this change in the UserModel type definition.

--- api/src/Models/UserModel.ts
+++ api/src/Models/UserModel.ts
@@ -14,11 +14,11 @@ export type User ={
     group_chat_list_ids: ObjectID[],
     troc_score?: string | null,
     phone: string,
-    friends_id: ObjectID[],
+    friends_id: Record<string, string>,
     friends_request_id: ObjectID[],
 }
 
-export type FilledUser = Omit<User, "_id" | "group_chat_list_ids" | "friends_id" | "password" | "friends_request_id"> & {_id: string, group_chat_list_ids: FilledChannel[], friends: string[], friends_request: string[], common_channels?: Channel[] | FilledChannel[], common_activity?: Activity[] | FilledActivity[] };
+export type FilledUser = Omit<User, "_id" | "group_chat_list_ids" | "friends_id" | "password" | "friends_request_id"> & {_id: string, group_chat_list_ids: FilledChannel[], friends: Record<string, string>, friends_request: string[], common_channels?: Channel[] | FilledChannel[], common_activity?: Activity[] | FilledActivity[] };
 
 export type PublicUser = Omit<FilledUser, "password" | "email" | "address" | "phone" | "group_chat_list_ids" | "friends" | "friends_request">
 
