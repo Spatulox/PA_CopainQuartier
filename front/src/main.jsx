6652465ce50c3a255bb097d69f130c9710e82310
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2025-06-24 15:21:03 +0200

    wip

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -3,18 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './app/css/index.css'
 import App from './app/App'
 
-import "./app/components/Chat/Chat.css"
-import "./app/components/Trocs/Trocs.css"
-import "./app/components/shared/footer.css"
-import "./app/components/shared/header.css"
-import "./app/components/HomePage/Home.css"
-import "./app/components/Activity/Activity.css"
-
-
-
-
-
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
