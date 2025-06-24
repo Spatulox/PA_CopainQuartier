af77de6a152e0d7ee4d47ec9d8f983e70bf348f1
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2025-06-24 15:33:44 +0200

    correctif header

--- front/src/app/components/shared/header.tsx
+++ front/src/app/components/shared/header.tsx
@@ -4,8 +4,8 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from './auth-context';
 import { ApiClient } from '../../../api/client';
 import { Route } from '../../constantes';
-
 import "../Popup/Popup.css"
+import "./header.css"
 
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
