3e23cc14d75be70aa1b8ac00bde52b1d26799847
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2025-06-25 14:43:47 +0200

    add button

--- src/main/java/com/example/scraper/ui/MainApp.java
+++ src/main/java/com/example/scraper/ui/MainApp.java
@@ -5,19 +5,23 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import com.example.scraper.ui.PluginHelper;
+
 
 public class MainApp extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button concertButton = new Button("Voir les concerts");
-        Button museeButton = new Button("Voir les musées");
-        Button spectacleButton = new Button("Voir les spectacles");
+        Button museeButton = new Button(" Voir les musées");
+        Button spectacleButton = new Button(" Voir les spectacles");
+        Button pluginButton = new Button("Ajouter un plugin");
 
         concertButton.setOnAction(e -> showCategory(primaryStage, "concert"));
         museeButton.setOnAction(e -> showCategory(primaryStage, "musee"));
         spectacleButton.setOnAction(e -> showCategory(primaryStage, "spectacle"));
+        pluginButton.setOnAction(e -> PluginHelper.showPluginForm(primaryStage));
 
-        VBox menu = new VBox(20, concertButton, museeButton, spectacleButton);
+        VBox menu = new VBox(20, concertButton, museeButton, spectacleButton, pluginButton);
         menu.setStyle("-fx-alignment: center; -fx-padding: 40;");
 
         Scene menuScene = new Scene(menu, 800, 600);
