2f57e0548680893b4ab78a9fa8a2d15a9e454ccb
Spatulox <M78stormtrooper@laposte.net>
2025-06-25 14:45:02 +0200

    Adds Multer configuration options
    Adds Multer configuration options for different image uploads,
    including activity, publication, troc, and general uploads,
    with destination paths, filename generation, file filtering, and size limits.

--- api/src/Utils/multer.ts
+++ api/src/Utils/multer.ts
@@ -2,19 +2,77 @@ import { diskStorage } from "multer";
 import { extname } from "path";
 import { Request } from "express-serve-static-core";
 
+export enum ImgFilepath {
+  activity = "img/activity",
+  publication = "img/publication",
+  troc = "img/troc",
+  uploads = "uploads"
+}
+
 type Callback = (error: null | Error, val: any) => void;
 type File = Express.Multer.File;
 
-export const uploadsOptions = {
+export const activityUploadsOptions = {
   storage: diskStorage({
-    destination: "uploads",
+    destination: ImgFilepath.activity,
     filename: (req, file, cb) => {
       cb(null, Date.now() + extname(file.originalname));
     }
   }),
   fileFilter: (req: Request, file: File, cb: Callback) => {
     // Ok Type (HTML / PNG, ETC...)
-    const isValid = true; // ou ta logique
+    const isValid = true;
+    cb(null, isValid);
+  },
+  limits: {
+    fileSize: 20 * 1024 * 1024 // 20 Mo
+  }
+};
+
+export const trocUploadsOptions = {
+  storage: diskStorage({
+    destination: ImgFilepath.troc,
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + extname(file.originalname));
+    }
+  }),
+  fileFilter: (req: Request, file: File, cb: Callback) => {
+    // Ok Type (HTML / PNG, ETC...)
+    const isValid = true;
+    cb(null, isValid);
+  },
+  limits: {
+    fileSize: 20 * 1024 * 1024 // 20 Mo
+  }
+};
+
+export const publicationUploadsOptions = {
+  storage: diskStorage({
+    destination: ImgFilepath.publication,
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + extname(file.originalname));
+    }
+  }),
+  fileFilter: (req: Request, file: File, cb: Callback) => {
+    // Ok Type (HTML / PNG, ETC...)
+    const isValid = true;
+    cb(null, isValid);
+  },
+  limits: {
+    fileSize: 20 * 1024 * 1024 // 20 Mo
+  }
+};
+
+export const uploadsOptions = {
+  storage: diskStorage({
+    destination: ImgFilepath.uploads,
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + extname(file.originalname));
+    }
+  }),
+  fileFilter: (req: Request, file: File, cb: Callback) => {
+    // Ok Type (HTML / PNG, ETC...)
+    const isValid = true;
     cb(null, isValid);
   },
   limits: {
