5982a39043a0f603ca802d478fd2404afadbd70c
Spatulox <M78stormtrooper@laposte.net>
2025-06-26 12:07:04 +0200

    Adds mailing functionality
    Implements a newsletter sender using nodemailer.
    Configures SMTP transport with Gmail and environment variables.
    Adds a method to send emails to a list of recipients.

--- api/src/Utils/config.ts
+++ api/src/Utils/config.ts
@@ -1,14 +1,15 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-const { JWT_SALT, PASSWORD_SALT, MONGO_URL, } = process.env;
+const { JWT_SALT, PASSWORD_SALT, MONGO_URL, MAILER_PASSWORD } = process.env;
 
-if (!JWT_SALT || !PASSWORD_SALT || !MONGO_URL) {
-    throw new Error("Missing environment variables: JWT_SALT, PASSWORD_SALT or MONGO_URL");
+if (!JWT_SALT || !PASSWORD_SALT || !MONGO_URL || !MAILER_PASSWORD) {
+    throw new Error("Missing environment variables: JWT_SALT, PASSWORD_SALT, MONGO_URL or MAILER_PASSWORD");
 }
 
 export const config = {
     jwtSalt: JWT_SALT,
     passwordSalt: PASSWORD_SALT,
-    mongoUrl: MONGO_URL
+    mongoUrl: MONGO_URL,
+    mailerParssword : MAILER_PASSWORD
 };
