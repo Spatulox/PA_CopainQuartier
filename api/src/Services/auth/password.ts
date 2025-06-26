d829ded75282a7f66e85ffc571be145a0c72b978
Spatulox <M78stormtrooper@laposte.net>
2025-06-26 11:30:34 +0200

    Updates auth/password to use async function
    Ensures password hashing is handled asynchronously.

--- api/src/Services/auth/password.ts
+++ api/src/Services/auth/password.ts
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 import { config } from '../../Utils/config';
 
-export function hashPassword(password: string): Promise<string> {
+export async function hashPassword(password: string): Promise<string> {
 	return bcrypt.hash(password, config.passwordSalt);
 }
