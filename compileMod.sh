044387ba8ba3d9c2dca49892c7bc12f2c2ea8bdd
Spatulox <M78stormtrooper@laposte.net>
2025-07-01 16:35:47 +0200

    Updates compileMod.sh
    Adds Jackson library dependencies to the compilation process.
    
    Includes new common source files for managing internet requests and database interactions.

--- compileMod.sh
+++ compileMod.sh
@@ -6,6 +6,9 @@ JSOUP_JAR="$HOME/.m2/repository/org/jsoup/jsoup/1.17.2/jsoup-1.17.2.jar"
 JAVAFX_CONTROLS="$HOME/.m2/repository/org/openjfx/javafx-controls/21.0.1/javafx-controls-21.0.1-linux.jar"
 JAVAFX_BASE="$HOME/.m2/repository/org/openjfx/javafx-base/21.0.1/javafx-base-21.0.1-linux.jar"
 JAVAFX_GRAPHICS="$HOME/.m2/repository/org/openjfx/javafx-graphics/21.0.1/javafx-graphics-21.0.1-linux.jar"
+JACKSON_CORE="$HOME/.m2/repository/com/fasterxml/jackson/core/jackson-core/2.13.0/jackson-core-2.13.0.jar"
+JACKSON_DATABIND="$HOME/.m2/repository/com/fasterxml/jackson/core/jackson-databind/2.13.0/jackson-databind-2.13.0.jar"
+
 
 BUILD_DIR="build"
 PLUGINS_DIR="plugins/mod"
@@ -17,9 +20,10 @@ PLUGINS=("ParisConcert" "ParisMusee" "ParisSpectacle" "Test")
 # Fichiers communs à tous les plugins
 COMMON_SOURCES=(
     "src/main/java/com/example/scraper/core/Plugin.java"
-    "src/main/java/com/example/scraper/pluginutils/PluginScrap.java"
+    "src/main/java/com/example/scraper/pluginutils/InternetRequest.java"
     "src/main/java/com/example/scraper/themeutils/ThemeManager.java"
     "src/main/java/com/example/scraper/core/ThemePlugin.java"
+    "src/main/java/com/example/scraper/core/Database.java"
 )
 
 for PLUGIN in "${PLUGINS[@]}"; do
@@ -27,7 +31,7 @@ for PLUGIN in "${PLUGINS[@]}"; do
     PLUGIN_SOURCE="src/main/java/com/example/scraper/plugins/$PLUGIN.java"
 
     # Compilation
-    javac -d "$BUILD_DIR" -cp "$JSOUP_JAR:$JAVAFX_CONTROLS:$JAVAFX_BASE:$JAVAFX_GRAPHICS" \
+    javac -d "$BUILD_DIR" -cp "$JSOUP_JAR:$JAVAFX_CONTROLS:$JAVAFX_BASE:$JAVAFX_GRAPHICS:$JACKSON_CORE:$JACKSON_DATABIND" \
         "${COMMON_SOURCES[@]}" "$PLUGIN_SOURCE"
 
     # Crée le dossier META-INF/services pour le plugin
