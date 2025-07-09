3b59366849e9f97d176a630066d3539775bd6c82
Spatulox <M78stormtrooper@laposte.net>
2025-07-09 10:59:05 +0200

    Update errors Ensures errors is not empty
    Adds a check to ensure the `errors` array has elements
    before rendering.
    
    Updates error console log messages

--- front/src/app/components/shared/errors.tsx
+++ front/src/app/components/shared/errors.tsx
@@ -9,22 +9,23 @@ function Errors({errors}: ErrorProp){
     console.log("Errors", errors)
 
     try{
-        if(errors && Array.isArray(errors)){
+        if(errors && Array.isArray(errors) && errors.length > 0){
             return <>
                 <p>Multiples erreurs</p>
                 {errors.map((err) => {
                     err.message
                 })}
                 </>
-        }else if(typeof errors === "string"){
+        } else if(typeof errors === "string"){
             return <p>Erreur</p>
         } else if(errors && "message" in errors){
             return <p>{errors?.message}</p>
-        }else {
+        } else {
+            {console.log(errors)}
             <p>Erreur dans les erreurs (ou pas d'erreur, wth) (welp)</p>
         }
     } catch (e) {
-        console.error("Error rendering Errors component:", e);
+        console.error("Error when rendering \"Errors\" component:", e);
     }
 }
 
